02ad62bbf70af23ccbec90ded7f1a9a9844c4d01
Sharmin-jahan-sathi <sharminjahan12th@gmail.com>
2024-11-02 00:42:31 +0600

    Add files via upload

---
+++ Class-26/Code/Work_of_Laser_Sensor/Work_of_Laser_Sensor.ino
@@ -0,0 +1,11 @@
+// Code for blinking a laser module using Arduino
+// Author: Oyvind N. Dahl
+// Website: https://www.build-electronic-circuits.com/
+
+void setup() {
+  pinMode(7, OUTPUT);  
+}
+
+void loop() {
+  digitalWrite(7, HIGH);
+}
